baf3023a29c806f722efa29680382979e179d121
soyluisarrieta <luisarrieta796@gmail.com>
2025-06-06 09:04:54 -0500

    Add Tailwind CSS support

--- 31.fabricjs-first-time/src/App.tsx
+++ 31.fabricjs-first-time/src/App.tsx
@@ -2,7 +2,7 @@ import ImageEditor from './components/ImageEditor';
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen bg-gray-100">
       <h1>Editor de Imágenes Básico</h1>
       <ImageEditor />
     </div>
