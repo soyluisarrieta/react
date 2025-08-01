a75e26de1869e049fad4244866089722eba76012
Luis Arrieta <luisarrieta796@gmail.com>
2025-08-01 14:39:32 -0500

    Add calendar header UI with navigation buttons

--- 33.dnd-kit-week-calendar/src/App.tsx
+++ 33.dnd-kit-week-calendar/src/App.tsx
@@ -1,9 +1,22 @@
 import { Button } from '@/components/ui/button'
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
 
 export default function App () {
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Hola ShadCN + Tailwind</Button>
+    <div className="p-6 max-w-7xl mx-auto">
+      <header className="flex items-center justify-between">
+        <h2 className="text-3xl font-semibold">Octubre, 2025</h2>
+        <div className='flex items-center gap-1'>
+          <Button variant='outline' size='icon'>
+            <ChevronLeftIcon />
+          </Button>
+          <Button variant='outline'>Hoy</Button>
+          <Button variant='outline' size='icon'>
+            <ChevronRightIcon />
+          </Button>
+        </div>
+      </header>
     </div>
   )
 }
