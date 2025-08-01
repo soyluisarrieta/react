ba958e2f6f6760b2e20fa8bfd9f1af86fdf96a83
Luis Arrieta <luisarrieta796@gmail.com>
2025-08-01 14:18:42 -0500

    Install @soyluisarrieta eslint rules and git ignore pnpm-lock.yaml

--- 33.dnd-kit-week-calendar/src/App.tsx
+++ 33.dnd-kit-week-calendar/src/App.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
-export default function App() {
+export default function App () {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Hola ShadCN + Tailwind</Button>
